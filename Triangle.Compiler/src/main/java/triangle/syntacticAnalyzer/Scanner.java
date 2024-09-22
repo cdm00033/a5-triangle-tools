c4c3714f89d3d0d7fdc3d9fbad6ef39febc84c4a
unknown <cdm00033@students.stir.ac.uk>
2024-09-22 19:25:12 +0100

    Added # commenting

--- Triangle.Compiler/src/main/java/triangle/syntacticAnalyzer/Scanner.java
+++ Triangle.Compiler/src/main/java/triangle/syntacticAnalyzer/Scanner.java
@@ -69,7 +69,8 @@ public final class Scanner {
 		switch (currentChar) {
 		
 		// comment
-		case '!': 
+		case '!':
+		case '#':
 			takeIt();
 			
 			// the comment ends when we reach an end-of-line (EOL) or end of file (EOT - for end-of-transmission)
@@ -80,6 +81,7 @@ public final class Scanner {
 			break;
 
 		// whitespace
+		case '$':
 		case ' ':
 		case '\n':
 		case '\r':
@@ -256,7 +258,7 @@ public final class Scanner {
 
 		currentlyScanningToken = false;
 		// skip any whitespace or comments
-		while (currentChar == '!' || currentChar == ' ' || currentChar == '\n' || currentChar == '\r'
+		while (currentChar == '!' || currentChar == '#' || currentChar == ' ' || currentChar == '\n' || currentChar == '\r'
 				|| currentChar == '\t')
 			scanSeparator();
 
